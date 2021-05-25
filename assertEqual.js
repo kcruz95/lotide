328fa9898f2a34e6bf2bd17ef23c90aa469a7ee6
kcruz95 <kyle.cruz@hotmail.ca>
2021-05-24 20:58:52 -0700

    lotide

---
+++ assertEqual.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+console.log(assertEqual);
+
+//🛑🛑🛑 Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
+//✅✅✅ Assertion Passed: 1 === 1
+
+//Template Literal Answer
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+}
---
+++ template-literals.js
@@ -0,0 +1,9 @@
+const name = 'Gilberto';
+console.log('Oi, ' + name + '!'); //logs: Oi, Gilberto!
+
+// Template literals (aka template strings) allow us to simplify this process while simultaneously being faster and using up less computer memory!
+
+//The next example greets Gilberto using the template literals syntax.
+
+const name = 'Gilberto';
+console.log(`Oi, ${name}!`);
