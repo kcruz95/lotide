1e89de279372ff51a5a4ede3773af9becb069e83
kcruz95 <kyle.cruz@hotmail.ca>
2021-05-26 23:46:46 -0700

    letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,16 @@
+const eqArrays = function(array1, array2) {
+  let correct = true;
+  if (array1.length !== array2.length) {
+  return false;
+  } for (let i = 0; i < array1.length; i++) {
+  if (array1[i] !== array2[i]) {
+  return false; 
+  }
+  } return true;
+  };
+
+  return correct;
+
 const assertArraysEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -33,7 +46,7 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-assertArraysEqual(letterPositions("hello").h, [0]);
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("hello").o, [4]);
+assertArraysEqual(eqArrays(letterPositions("hello").h, [0]), true);
+assertArraysEqual(eqArrays(letterPositions("hello").e, [1]), true);
+assertArraysEqual(eqArrays(letterPositions("hello").l, [2, 3]), true);
+assertArraysEqual(eqArrays(letterPositions("hello").o, [4]), true);
