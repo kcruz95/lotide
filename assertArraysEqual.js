a605d00e5c108b740ea54ca5f791169506233812
kcruz95 <kyle.cruz@hotmail.ca>
2021-05-27 23:37:29 -0700

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,17 @@
 const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArray((arrayOne, arrayTwo))) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+/* old code
+const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (eqArrays ((arrayOne, arrayTwo))) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
   }
 }
+*/
