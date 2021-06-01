5e1c0be4117dcac24ce6495f9632249b26964f60
kcruz95 <kyle.cruz@hotmail.ca>
2021-05-31 17:33:17 -0700

    tail.js

--- tail.js
+++ tail.js
@@ -1,10 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const tail = (array) => array.slice(1);
 
@@ -16,3 +17,5 @@ assertEqual(result, ["Lighthouse", "Labs"]);
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = assertEqual;
