6159c3585760b0d79b318fa9312c74539d3ae6bb
kcruz95 <kyle.cruz@hotmail.ca>
2021-05-31 19:04:22 -0700

    test/eqArraysTest.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,13 @@
+const eqArrays = require('./eqArrays');
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } for (let i = 0; i < arrayOne.length; i = i + 1) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+
+module.exports = eqArrays;
