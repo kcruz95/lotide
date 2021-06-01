14d638b491dd8f50d9d6d5ceddef1a7796793377
kcruz95 <kyle.cruz@hotmail.ca>
2021-05-31 22:21:31 -0700

    test/headtest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,17 +1,30 @@
-const assertEqual = require('./assertEqual');
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
-//   }
-// }
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const head = function(array) {
-  array[0]
-};
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
 
-module.exports = assertEqual;
+// const assertEqual = require('./assertEqual');
+// // const assertEqual = function(actual, expected) {
+// //   if (actual === expected) {
+// //     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+// //   } else {
+// //     console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+// //   }
+// // }
+
+// const head = function(array) {
+//   array[0]
+// };
+
+// // assertEqual(head([5,6,7]), 5);
+// // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// module.exports = assertEqual;
