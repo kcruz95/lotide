8cc6589cfe2c4d75a9b12793713b38c2fdf88faa
kcruz95 <kyle.cruz@hotmail.ca>
2021-05-31 17:19:32 -0700

    head.js

--- head.js
+++ head.js
@@ -1,14 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
-  }
-}
+const assertEqual = require('./assertEqual');
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+//   }
+// }
 
 const head = function(array) {
   array[0]
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
