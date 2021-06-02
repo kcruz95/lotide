f600145c1473f060ce43d44cb4d313d8dfd4c6bf
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-02 00:44:23 -0700

    findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+/*
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -5,6 +7,7 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
   }
 };
+*/
 
 const findKeyByValue = function(obj, val) { //scans the obj & returns the 1st key w/ a value
   //const keyArr = Object.keys(obj); //break obj into keys and values (key:value)
@@ -23,3 +26,5 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+module.exports = assertEqual;
