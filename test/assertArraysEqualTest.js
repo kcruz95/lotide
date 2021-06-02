ad69b7dc345eb2e1aa0422c6e710e83d0a2855f9
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-02 00:53:51 -0700

    test/assertArraysEqualTest.js

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,21 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArray((arrayOne, arrayTwo))) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+/* old code
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays ((arrayOne, arrayTwo))) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+}
+*/
+
+module.exports = eqArrays;
