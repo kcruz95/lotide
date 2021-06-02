cb4f2e8dc4d3fe232476b028552099686fcc3fb5
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-02 01:10:51 -0700

    test/mapTest.js

---
+++ test/mapTest.js
@@ -0,0 +1,49 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArray = require('./eqArray');
+
+/*
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArray((arrayOne, arrayTwo))) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+*/
+
+const map = function(array, callback) {
+  // console.log('array: ', array);
+  // console.log('callback: ', callback);
+  const results = [];
+  for (let item of array) {
+    // console.log('item BEFORE: ', item);
+    // console.log('item AFTER: ', callback(item));
+    // console.log('---');
+    results.push(callback(item));
+  }
+  return results;
+}
+
+// example 1
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+// example 2
+const array1 = [4, 8, 16, 32];
+const map1 = array1.map(x => x / 2);
+console.log(map1);
+
+module.exports = assertEqual;
+module.exports = eqArrays;
